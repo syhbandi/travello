28a42fdee0a6343c8109c64a3104417caf112cc5
syhbandi <saboonlbk@gmail.com>
2025-02-17 23:29:42 +0800

    Enhance Navbar with scroll effect and dynamic styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,35 @@
+import { useEffect, useRef } from "react";
 import logo from "../assets/images/travello.png";
 
 const Navbar = () => {
+  const navRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      console.log("udah 50 nih");
+      if (window.scrollY > 50) {
+        navRef.current?.classList.add("bg-white", "shadow");
+      } else {
+        navRef.current?.classList.remove("bg-white", "shadow");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+
   return (
-    <nav className="fixed inset-0 py-5">
+    <nav
+      className="fixed w-full py-5 z-10 transition-all ease-in-out duration-300"
+      ref={navRef}
+    >
       <div className="container mx-auto px-5 max-w-7xl flex items-center justify-between">
         <a href="/">
           <img src={logo} className="w-[100px] h-auto" alt="Travello" />
         </a>
 
-        <div className="items-center gap-10 font-poppins hidden lg:flex">
+        <div className="items-center gap-10 font-poppins hidden md:flex">
           {["Destinations", "Hotels", "Flights", "Bookings", "Login"].map(
             (item, index) => (
               <a
